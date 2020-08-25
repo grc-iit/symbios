b5778347f4e736b4d519c2b46279d62150f1abc0
Jaime Cernuda <jcernudagarcia@hawk.iit.edu>
2020-08-24 19:03:49 -0500

    delete build folder

--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jcernuda/symbios/test
-# Build directory: /home/jcernuda/symbios/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("unit")
-subdirs("integration")
