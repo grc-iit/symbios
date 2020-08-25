219dfad15e8f1e5d93017bf73caec5abfef1aea2
Jaime Cernuda <jcernudagarcia@hawk.iit.edu>
2020-08-25 13:19:35 -0500

    Revert "delete build folder"
    This reverts commit b5778347f4e736b4d519c2b46279d62150f1abc0.

---
+++ build/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jcernuda/symbios/test
+# Build directory: /home/jcernuda/symbios/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("unit")
+subdirs("integration")
