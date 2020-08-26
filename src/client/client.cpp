d4c2913a85b51dea796a1edce933ad6814fa1423
jaime <jcernudagarcia@hawk.iit.edu>
2020-08-26 17:13:08 -0500

    Revert "Created Server"
    This reverts commit 3d0db8acabfc73013bcb5f3d68e7d2b5442b22f3.

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -3,15 +3,3 @@
 //
 
 #include "../../include/symbios/client.h"
-#include <basket.h>
-#include <iostream>
-#include <mpi.h>
-#include "../server/configuration_manager.h"
-#include "../server/data_structures.h"
-
-void send_request(Request req) {
-    int my_server = 0;
-    basket::queue<Request> int_queue("SERVER_REQUEST_QUEUE",SYMBIOS_CONF->CLIENT_PORT);
-
-    int_queue.Push(req, my_server);
-}
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -2,20 +2,6 @@
 // Created by mani on 8/24/2020.
 //
 
-#include <mpi.h>
-#include <basket.h>
-#include "server.h"
-#include "Daemon.h"
-#include "configuration_manager.h"
-
-int main(int argc, char* argv[]){
-    MPI_Init_thread(&argc,&argv,true,NULL);
-    MPI_Barrier(MPI_COMM_WORLD);
-    if(argc > 1) SYMBIOS_CONF->CONFIGURATION_FILE=argv[1];
-    BASKET_CONF->BACKED_FILE_DIR=SYMBIOS_CONF->CHRONOPLAYER_DIR;
-    SYMBIOS_CONF->ConfigureChronoplayerServer();
-
-    symbios::Daemon<symbios::Server>();
-
-    while(true) sleep(1);
+int main(int argc, char * argv[]){
+    return 0;
 }
