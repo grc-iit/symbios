cad8abc99c1a8b38b19ab2ab1ac44a63ee7ff17f
Jie Ye <jye20@hawk.iit.edu>
2020-08-29 20:50:25 -0500

    Add mongo io implementation and redis cluster running scripts

--- src/symbios/io_clients/mongo_io.cpp
+++ src/symbios/io_clients/mongo_io.cpp
@@ -15,14 +15,14 @@
 
 void MongoIOClient::Read(Data &source, Data &destination) {
     try {
-        auto builder = bason::builder::stream::document{};
+        auto builder = bsoncxx::builder::stream::document{};
         bsoncxx::view_or_value<bsoncxx::document::view, bsoncxx::document::value> query_doc_value = builder
                 << "_id" << source.id_.c_str()
                 << bsoncxx::builder::stream::finalize;
         bsoncxx::stdx::optional<bsoncxx::document::value> result = coll.find_one(query_doc_value);
         if(result){
             // get the result
-            std::string value = bsoncxx::to_json(*maybe_result);
+            std::string value = bsoncxx::to_json(result->view());
             std::string::size_type value_size = value.length();
             if(source.position_ + source.data_size_ > value_size){
                 throw ErrorException(READ_MONGO_POSITION_OR_SIZE_FAILED);
@@ -43,21 +43,21 @@ void MongoIOClient::Read(Data &source, Data &destination) {
 
 void MongoIOClient::Write(Data &source, Data &destination) {
     try {
-        auto builder = bason::builder::stream::document{};
+        auto builder = bsoncxx::builder::stream::document{};
         bsoncxx::view_or_value<bsoncxx::document::view, bsoncxx::document::value> query_doc_value = builder
                 << "_id" << source.id_.c_str()
                 << bsoncxx::builder::stream::finalize;
         bsoncxx::stdx::optional<bsoncxx::document::value> result = coll.find_one(query_doc_value);
         if(result){
             // The key has been existed in Mongodb
-            std::string old_value = bsoncxx::to_json(*maybe_result);
-            std::string::size_type old_value_size = value.length();
+            std::string old_value = bsoncxx::to_json(result->view());
+            std::string::size_type old_value_size = old_value.length();
             if (source.data_size_ >= old_value_size){
                 std::string new_value = std::string((char*)source.buffer_ + source.position_, source.data_size_);
-                auto builder = bason::builder::stream::document{};
+                auto builder = bsoncxx::builder::stream::document{};
                 bsoncxx::document::value insert_doc_value = builder
                         << "_id" << source.id_.c_str()
-                        << source.id_.c_str() << new_value
+                        << std::string(source.id_.c_str()) << new_value
                         << bsoncxx::builder::stream::finalize;
                 coll.insert_one(insert_doc_value.view());
                 destination.data_size_ = source.data_size_;
@@ -66,10 +66,10 @@ void MongoIOClient::Write(Data &source, Data &destination) {
                 // update the old_value
                 memcpy((void*)old_value.c_str(), (const void*)((char*)source.buffer_ + source.position_), source.data_size_);
                 // put the updated data back
-                auto builder = bason::builder::stream::document{};
+                auto builder = bsoncxx::builder::stream::document{};
                 bsoncxx::document::value insert_doc_value = builder
                         << "_id" << source.id_.c_str()
-                        << source.id_.c_str() << new_value
+                        << std::string(source.id_.c_str()) << old_value
                         << bsoncxx::builder::stream::finalize;
                 coll.insert_one(insert_doc_value.view());
                 destination.data_size_ = source.data_size_;
@@ -78,10 +78,10 @@ void MongoIOClient::Write(Data &source, Data &destination) {
         else {
             // The key isn't exist in redis cluster
             std::string value = std::string((char*)source.buffer_ + source.position_, source.data_size_);
-            auto builder = bason::builder::stream::document{};
+            auto builder = bsoncxx::builder::stream::document{};
             bsoncxx::document::value insert_doc_value = builder
                     << "_id" << source.id_.c_str()
-                    << source.id_.c_str() << value
+                    << std::string(source.id_.c_str()) << value
                     << bsoncxx::builder::stream::finalize;
             coll.insert_one(insert_doc_value.view());
             destination.data_size_ = source.data_size_;
