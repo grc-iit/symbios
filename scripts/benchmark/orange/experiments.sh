0164c53cbc28a140c7ded2138923e298b6f3f32e
lukemartinlogan <lukemartinlogan@gmail.com>
2020-08-31 05:06:00 -0500

    Add some feedback to bash script

--- scripts/benchmark/orange/experiments.sh
+++ scripts/benchmark/orange/experiments.sh
@@ -11,9 +11,12 @@ build_dir=$7
 client_dir=/mnt/nvme/${USER}/write/generic
 PROCS=(1 2 4 8 16 32 40)
 
+echo "Deploying"
 bash ${script_root}/deploy.sh ${conf_file} /mnt/${dev_type}/${USER}/orangefs ${client_dir} ${server_hostfile} ${client_hostfile}
+echo "Deployed"
 cd ${build_dir}
 for PROC in ${PROCS[@]}; do
+  echo "Tests for NPROCS=${PROC}"
   ctest --verbose -R prealloc_${PROC}_orangefs_*
   ctest --verbose -R aresbm_io_${PROC}_orangefs_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_${conf_id}
   ctest --verbose -R aresbm_md_${PROC}_orangefs_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_${conf_id}
@@ -21,4 +24,6 @@ for PROC in ${PROCS[@]}; do
   #ctest -N -R aresbm_io_${PROC}_orangefs_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_${conf_id}
   #ctest -N -R aresbm_md_${PROC}_orangefs_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_${conf_id}
 done
+echo "Terminating"
 bash ${script_root}/terminate.sh ${conf_file} /mnt/${dev_type}/${USER}/orangefs ${client_dir} ${server_hostfile} ${client_hostfile}
+echo "Terminated"
