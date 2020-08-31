104ecfcbc5671c49b61ce5d9d4764e3a873fd123
lukemartinlogan <lukemartinlogan@gmail.com>
2020-08-31 04:41:05 -0500

    Add deploy, test, terminate loop

--- scripts/benchmark/orange/experiments.sh
+++ scripts/benchmark/orange/experiments.sh
@@ -4,13 +4,14 @@ dev_type=$1
 conf_id=$2
 conf_file=$3
 server_hostfile=$4
-script_root=$5
-build_dir=$6
+server_hostfile=$5
+script_root=$6
+build_dir=$7
 
 client_dir=/mnt/nvme/${USER}/write/generic
 PROCS=(1 2 4 8 16 32 40)
 
-bash ${script_root}/deploy.sh ${conf_file} /mnt/${dev_type}/${USER}/orangefs ${client_dir} ${server_hostfile}
+bash ${script_root}/deploy.sh ${conf_file} /mnt/${dev_type}/${USER}/orangefs ${client_dir} ${server_hostfile} ${client_hostfile}
 cd ${build_dir}
 for PROC in ${PROCS[@]}; do
   ctest --verbose -R prealloc_${PROC}_orangefs_*
@@ -20,4 +21,4 @@ for PROC in ${PROCS[@]}; do
   #ctest -N -R aresbm_io_${PROC}_orangefs_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_${conf_id}
   #ctest -N -R aresbm_md_${PROC}_orangefs_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_${conf_id}
 done
-bash ${script_root}/terminate.sh ${conf_file} /mnt/${dev_type}/${USER}/orangefs ${client_dir} ${server_hostfile}
+bash ${script_root}/terminate.sh ${conf_file} /mnt/${dev_type}/${USER}/orangefs ${client_dir} ${server_hostfile} ${client_hostfile}
