b16533a14161ac8e7ea7a98aadefcd94c91bcb8c
lukemartinlogan <lukemartinlogan@gmail.com>
2020-08-31 20:53:33 -0500

    Use less restrictive regex for ctest

--- scripts/benchmark/orange/experiments.sh
+++ scripts/benchmark/orange/experiments.sh
@@ -12,7 +12,6 @@ client_dir=/mnt/nvme/${USER}/write/generic
 PROCS=(1 2 4 8 16 32 40)
 
 echo "Deploying"
-echo "${script_root}/deploy.sh ${conf_file} /mnt/${dev_type}/${USER}/orangefs ${client_dir} ${server_hostfile} ${client_hostfile}"
 bash ${script_root}/deploy.sh ${conf_file} /mnt/${dev_type}/${USER}/orangefs ${client_dir} ${server_hostfile} ${client_hostfile}
 echo "Deployed"
 cd ${build_dir}
@@ -20,12 +19,9 @@ for PROC in ${PROCS[@]}; do
   echo "Tests for NPROCS=${PROC}"
   rm -rf ${client_dir}/*
   ctest --verbose -R prealloc_${PROC}_orangefs_*
-  ctest --verbose -R aresbm_io_${PROC}_orangefs_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_${conf_id}
-  ctest --verbose -R aresbm_md_${PROC}_orangefs_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_${conf_id}
+  ctest --verbose -R aresbm_io_${PROC}_orangefs_[a-zA-Z0-9.]+_[a-zA-Z0-9.]+_[a-zA-Z0-9.]+_[a-zA-Z0-9.]+_[a-zA-Z0-9.]+_[a-zA-Z0-9.]+_[a-zA-Z0-9.]+_${conf_id}
+  ctest --verbose -R aresbm_md_${PROC}_orangefs_[a-zA-Z0-9.]+_[a-zA-Z0-9.]+_[a-zA-Z0-9.]+_${conf_id}
   rm -rf ${client_dir}/*
-  #ctest -N -R prealloc_${PROC}_orangefs_*
-  #ctest -N -R aresbm_io_${PROC}_orangefs_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_${conf_id}
-  #ctest -N -R aresbm_md_${PROC}_orangefs_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_${conf_id}
 done
 echo "Terminating"
 bash ${script_root}/terminate.sh ${conf_file} /mnt/${dev_type}/${USER}/orangefs ${client_dir} ${server_hostfile} ${client_hostfile}
