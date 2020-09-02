d38e0ec28177a7a05ac89b2a78b47a8c8a44c0b7
lukemartinlogan <lukemartinlogan@gmail.com>
2020-09-02 10:15:19 -0500

    Add error prog

--- test/unit/storage_cost_predictor/test.cpp
+++ test/unit/storage_cost_predictor/test.cpp
@@ -11,6 +11,7 @@ private:
     void VerifyArgs(void) {
         AssertOptIsSet("-metrics");
         AssertOptIsSet("-model");
+        AssertOptIsSet("-num_req");
     }
 
 public:
@@ -19,6 +20,7 @@ public:
         std::cout << std::endl;
         std::cout << "-metrics [string]: path to metrics file" << std::endl;
         std::cout << "-model [string]: path to model file" << std::endl;
+        std::cout << "-num_req [size]: The number of requests to run" << std::endl;
         std::cout << "" << std::endl;
     }
 
@@ -33,16 +35,18 @@ public:
 int main(int argc, char * argv[]) {
     MPI_Init(&argc,&argv);
     int rank=0, nprocs = 1;
+    size_t num_reqs = 0;
 
     SCCArgs args(argc, argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
     std::shared_ptr<StorageCostPredictor> a = basket::Singleton<StorageCostPredictor>::GetInstance();
     std::string config = "asdf";
+    num_reqs = args.Get;
     a->LoadMetrics(rank, nprocs, args.GetStringOpt("-metrics"), args.GetStringOpt("-model"));
-    a->Feedback(10*(rank+1), 25*(rank+1), 25*(rank+1), config);
-    a->Feedback(20*(rank+1), 50*(rank+1), 50*(rank+1), config);
-    a->Feedback(30*(rank+1), 75*(rank+1), 75*(rank+1), config);
+    for(size_t i =0; i < num_reqs; ++i) {
+        a->Feedback(10*(rank+1), 25*(rank+1), 25*(rank+1), config);
+    }
     a->FitCommit();
     std::cout << a->Predict(100.0, 100.0, config) << std::endl;
     //MPI_Finalize();
