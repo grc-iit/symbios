c81302cdbe82007eb74150553412c01ead9fa24c
jaime <jcernudagarcia@hawk.iit.edu>
2020-09-03 20:23:05 -0500

    Changes to Mongo Scripts

--- scripts/distributed/MongoDB/deploy.sh
+++ scripts/distributed/MongoDB/deploy.sh
@@ -10,8 +10,9 @@ SERVER_HOSTFILE=${1}
 ROUTER_SERVER_HOSTFILE=${2}
 CONFIG_SERVER_COUNT=${3}
 MONGO_PATH=${4}
-DATABASE_NAME=${5}
-COLLECTION_NAME=${6}
+CONF_PATH=${5}
+DATABASE_NAME=${6}
+COLLECTION_NAME=${7}
 
 mongod_config_path=${MONGO_PATH}/mongod_config
 mongod_shard_path=${MONGO_PATH}/mongod_shard
@@ -35,29 +36,29 @@ echo -e "${CYAN}Router Servers :${NC}${ROUTER_SERVERS[*]}"
 
 echo -e "${GREEN}============ Deploying MongoDB ============${NC}"
 
-echo -e "${GREEN}Preparing config files${NC}"
-sed -i "s|clusterRole: .*|clusterRole: configsvr|"                  ${MONGO_PATH}/mongod_config.conf
-sed -i "s|replSetName: .*|replSetName: ${CONFIG_REPL_NAME}|"        ${MONGO_PATH}/mongod_config.conf
-sed -i "s|dbPath:.*|dbPath: ${mongod_config_path}|"                 ${MONGO_PATH}/mongod_config.conf
-sed -i "s|path: .*|path: ${MONGO_PATH}/mongod_config.log|"          ${MONGO_PATH}/mongod_config.conf
-sed -i "s|port: .*|port: ${MONGO_PORT}|"                            ${MONGO_PATH}/mongod_config.conf
-sed -i "s|dbPath:.*|dbPath: ${mongos_local_path}|"                  ${MONGO_PATH}/mongos.conf
-sed -i "s|path: .*|path: ${MONGO_PATH}/mongos.log|"                 ${MONGO_PATH}/mongos.conf
-sed -i "s|port: .*|port: ${MONGO_PORT}|"                            ${MONGO_PATH}/mongos.conf
-sed -i 's|clusterRole: .*|clusterRole: shardsvr|'                   ${MONGO_PATH}/mongod_shard.conf
-sed -i "s|dbPath:.*|dbPath: ${mongod_shard_path}|"                  ${MONGO_PATH}/mongod_shard.conf
-sed -i "s|path: .*|path: ${MONGO_PATH}/,mongod_shared.log|"         ${MONGO_PATH}/mongod_shard.conf
-sed -i "s|port: .*|port: ${MONGO_PORT}|"                            ${MONGO_PATH}/mongod_shard.conf
-
 mkdir -p ${mongod_config_path}
 mkdir -p ${mongod_shard_path}
 mkdir -p ${mongos_local_path}
+
+echo -e "${GREEN}Preparing config files${NC}"
+sed -i "s|clusterRole: .*|clusterRole: configsvr|"                  ${CONF_PATH}/mongod_config.conf
+sed -i "s|replSetName: .*|replSetName: ${CONFIG_REPL_NAME}|"        ${CONF_PATH}/mongod_config.conf
+sed -i "s|dbPath:.*|dbPath: ${mongod_config_path}|"                 ${CONF_PATH}/mongod_config.conf
+sed -i "s|path: .*|path: ${MONGO_PATH}/mongod_config.log|"          ${CONF_PATH}/mongod_config.conf
+sed -i "s|port: .*|port: ${MONGO_PORT}|"                            ${CONF_PATH}/mongod_config.conf
+sed -i "s|dbPath:.*|dbPath: ${mongos_local_path}|"                  ${CONF_PATH}/mongos.conf
+sed -i "s|path: .*|path: ${MONGO_PATH}/mongos.log|"                 ${CONF_PATH}/mongos.conf
+sed -i "s|port: .*|port: ${MONGO_PORT}|"                            ${CONF_PATH}/mongos.conf
+sed -i 's|clusterRole: .*|clusterRole: shardsvr|'                   ${CONF_PATH}/mongod_shard.conf
+sed -i "s|dbPath:.*|dbPath: ${mongod_shard_path}|"                  ${CONF_PATH}/mongod_shard.conf
+sed -i "s|path: .*|path: ${MONGO_PATH}/,mongod_shared.log|"         ${CONF_PATH}/mongod_shard.conf
+sed -i "s|port: .*|port: ${MONGO_PORT}|"                            ${CONF_PATH}/mongod_shard.conf
 
 echo -e "${GREEN}Starting config nodes${NC}"
 for config_server in "${CONFIG_SERVERS[@]}"
 do
 ssh "${config_server}" /bin/bash << EOF
-  mongod --config ${MONGO_PATH}/mongod_config.conf --fork &
+  mongod --config ${CONF_PATH}/mongod_config.conf --fork &
 EOF
 done
 
@@ -80,7 +81,7 @@ echo -e "${GREEN}Checking Shard Servers${NC}"
 for shard_server in "${SHARD_SERVERS[@]}"
 do
 ssh "${shard_server}" /bin/bash << EOF
-  sed -i -e 's|replSetName: .*|replSetName: ${SHARD_REPL_NAME}${step}|' -e 's|port: .*|port: ${SHARD_BASE_PORT}|' ${MONGO_PATH}/mongod_shard.conf
+  sed -i -e 's|replSetName: .*|replSetName: ${SHARD_REPL_NAME}${step}|' -e 's|port: .*|port: ${SHARD_BASE_PORT}|' ${CONF_PATH}/mongod_shard.conf
 EOF
   ((SHARD_BASE_PORT = SHARD_BASE_PORT + 1))
   ((step=step+1))
@@ -88,7 +89,7 @@ done
 for shard_server in "${SHARD_SERVERS[@]}"
 do
 ssh "${shard_server}" /bin/bash << EOF
-  mongod --config ${MONGO_PATH}/mongod_shard.conf --fork" &
+  mongod --config ${CONF_PATH}/mongod_shard.conf --fork" &
 EOF
 done
 wait
@@ -137,7 +138,7 @@ do
   mongos_cmd="${mongos_cmd}${config_server}:${MONGO_PORT},"
 done
 mongos_cmd=`echo ${mongos_cmd} | sed 's/,$/"/'`
-mongos_cmd="${mongos_cmd} --config ${MONGO_PATH}/mongos.conf --fork"
+mongos_cmd="${mongos_cmd} --config ${CONF_PATH}/mongos.conf --fork"
 echo $mongos_cmd
 for router_server in "${ROUTER_SERVERS[@]}"
 do
