1c7185efede20387589fe38ba01985d713c63818
jaime <jcernudagarcia@hawk.iit.edu>
2020-09-03 20:58:56 -0500

    Changes to Mongo Scripts

--- scripts/distributed/MongoDB/terminate.sh
+++ scripts/distributed/MongoDB/terminate.sh
@@ -11,22 +11,23 @@ SERVER_HOSTFILE=${1}
 CLIENT_HOSTFILE=${2}
 MONGO_PATH=${3}
 
-SERVERS=$(cat SERVER_HOSTFILE | awk '{print $1}')
-CLIENTS=$(cat CLIENT_HOSTFILE | awk '{print $1}')
+SERVERS=($(cat ${SERVER_HOSTFILE}))
+CLIENTS=($(cat ${CLIENT_HOSTFILE}))
 #echo -e "${GREEN}Remove all${NC}"
 #first_client="${CLIENTS[0]}"
 #mongo --host ${first_client} -p ${MONGO_PORT} < ${CWD}/clear_db.js
 
-echo -e "${CYAN}Servers :${NC}${SERVERS[*]}"
-echo -e "${CYAN}Router Servers :${NC}${CLIENTS[*]}"
-
 echo -e "${GREEN}Stopping MongoDB servers${NC}"
+echo -e "${CYAN}Servers :${NC}${SERVERS[*]}"
 for server in "${SERVERS[@]}"
 do
 ssh "${server}" /bin/bash << EOF
   pkill mongod
 EOF
 done
+
+echo -e "${GREEN}Stopping MongoDB Router servers${NC}"
+echo -e "${CYAN}Router Servers :${NC}${CLIENTS[*]}"
 for client in "${CLIENTS[@]}"
 do
 ssh "${client}" /bin/bash << EOF
